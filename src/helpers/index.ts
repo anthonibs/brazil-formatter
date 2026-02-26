1529f7062b9a1e37b0390cf8f0125441fa60f8ab
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2026-02-26 13:06:31 -0300

    feat(máscaras): adicionar funções para validação e remoção de caracteres especiais

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -6,3 +6,11 @@ export function onlyDigits(value: string): string {
 export function isRepeatedDigits(value: string): boolean {
 	return /^(\d)\1+$/.test(value);
 }
+
+export function isRepeatedAlphanumeric(value: string): boolean {
+	return /^(.)\1+$/.test(value);
+}
+
+export function removeSpecialCharacters(value: string): string {
+	return value.replace(/[^A-Z0-9]/gi, "").toUpperCase();
+}
